256f92526ea28987a057c254c3cbc82fd9f88f20
JeewanNextuple <jeewan.khatiwara@nextuple.com>
2024-01-29 09:56:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Table } from "antd";
+import axios from "axios";
+import styles from "./App.module.css";
 
-function App() {
+const App = () => {
+  const [question, setQuestion] = useState("");
+  const [response, setResponse] = useState(null);
+  const [tableData, setTableData] = useState([]);
+  const [columns, setColumns] = useState([]);
+  const [errorInParsing, setErrorInParsing] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.post("http://127.0.0.1:5000/ask", {
+        question,
+      });
+
+      setResponse(response.data);
+      setTableData(response.data);
+
+      const firstItem = response.data[0];
+      if (firstItem) {
+        const dynamicColumns = Object.keys(firstItem).map((key) => ({
+          title: key.toUpperCase(),
+          dataIndex: key,
+          key: key,
+        }));
+        setColumns(dynamicColumns);
+      }
+    } catch (error) {
+      console.error("Error asking question:", error.message);
+      setErrorInParsing(true);
+      setResponse(response);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className={styles.heading}>KEEPING THE LIGHTS ON!</h1>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <label className={styles.label}>
+            Input your statement
+            <input
+              type="text"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              className={styles.input}
+            />
+          </label>
+          <button type="submit" className={styles.button}>
+            Submit
+          </button>
+        </form>
+      </div>
+      {response && !errorInParsing && (
+        <Table
+          columns={columns}
+          dataSource={tableData}
+          pagination={false}
+          rowKey={() => Math.random()}
+        />
+      )}
+      {response && errorInParsing && response}
+    </>
   );
-}
+};
 
 export default App;
